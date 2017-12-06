ea799e43b4dff46616464ebc0a9fa545325ca15a
Saracen <SaracenOne@gmail.com>
2017-12-06 08:12:44 +0000

    Converted rect3 name back to aabb, added function to get bounding volume from sphere.

--- geometry_util.gd
+++ geometry_util.gd
@@ -1,10 +1,13 @@
 tool
 
-static func test_rect3_with_planes(p_rect3, p_planes):
+static func test_aabb_with_planes(p_aabb, p_planes):
 	for plane in p_planes:
-		if(plane.is_point_over(p_rect3.pos) and plane.is_point_over(p_rect3.end)):
+		if(plane.is_point_over(p_aabb.pos) and plane.is_point_over(p_aabb.end)):
 			return false
 	return true
 	
 static func get_cylinder_boundings_box(p_pos, p_cylinder):
 	return AABB(p_pos, Vector3(p_cylinder.get_radius(), p_cylinder.get_height() * 0.5, p_cylinder.get_radius()))
+	
+static func get_sphere_boundings_box(p_pos, p_sphere):
+	return AABB(p_pos, Vector3(p_sphere.get_radius(), p_sphere.get_radius(), p_sphere.get_radius()))
