b20afe93b8b878beb09fccb70f58e523372c2776
Saracen <SaracenOne@gmail.com>
2020-01-05 10:27:25 +0000

    Dedicated mutex lock class

---
+++ mutex_lock.gd
@@ -0,0 +1,12 @@
+extends Reference
+
+var mutex : Mutex = null
+
+func _init(p_mutex : Mutex):
+	mutex = p_mutex
+	mutex.lock()
+	
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_PREDELETE:
+			mutex.unlock()
