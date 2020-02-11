5e48a3754ba574c265b241a5d544081c7cffe149
Saracen <SaracenOne@gmail.com>
2020-02-11 09:03:07 +0000

    Added bitset

---
+++ bitset.gd
@@ -0,0 +1,29 @@
+extends Reference
+
+var bitset : PoolByteArray = PoolByteArray()
+var first_available_index : int = 0
+
+func get_bit(p_bit_index : int) -> bool:
+	var byte_position : int = p_bit_index / 8
+	var remainder : int = p_bit_index % 8
+
+	return ((bitset[byte_position] & (1 << remainder)) >> remainder) == 1
+
+func set_bit(p_bit_index : int, p_enable : bool) -> void:
+	var byte_position : int = p_bit_index / 8
+	var remainder : int = p_bit_index % 8
+	
+	if p_enable == true:
+		bitset[byte_position] |= (1 << remainder)
+	else:
+		bitset[byte_position] &= ~(1 << remainder)
+		
+func _init(p_size : int, p_enabled : bool) -> void:
+	bitset = PoolByteArray()
+	bitset.resize(((p_size) - 1 / 8) + 1)
+	if p_enabled:
+		for i in range(0, bitset.size()):
+			bitset[i] = 1
+	else:
+		for i in range(0, bitset.size()):
+			bitset[i] = 0
