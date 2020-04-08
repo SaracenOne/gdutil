c6dbd268a75ee0e8ecb853c8354503591fdb6bc1
Saracen <SaracenOne@gmail.com>
2020-04-08 03:30:28 +0100

    Add stopgap refpool class

---
+++ ref_pool.gd
@@ -0,0 +1,9 @@
+tool
+extends Reference
+
+# Stopgap, used until 4.0
+
+var pool_byte_array : PoolByteArray = PoolByteArray()
+
+func _init(p_pool_byte_array : PoolByteArray) -> void:
+	pool_byte_array = p_pool_byte_array
