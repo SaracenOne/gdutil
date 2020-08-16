f189cce83d7da3d889bb2d392acf01868a25665a
Saracen <SaracenOne@gmail.com>
2020-08-16 22:14:13 +0100

    Add printed messages for initialisation and destruction.

--- gdutil_plugin.gd
+++ gdutil_plugin.gd
@@ -1,6 +1,19 @@
 tool
 extends EditorPlugin
 
+func _init():
+	print("Initialising GDUtil plugin")
+
+
+func _notification(p_notification: int):
+	match p_notification:
+		NOTIFICATION_PREDELETE:
+			print("Destroying GDUtil plugin")
+
+
+func get_name() -> String:
+	return "GDUtil"
+
 
 func _enter_tree() -> void:
 	add_autoload_singleton("ConnectionUtil", "res://addons/gdutil/connection_util.gd")
